dd1e60e78ce81d5a899c6dd7f557a22db703c9f0
jsswrm <jessewarman23@gmail.com>
2025-03-07 14:04:12 +0100

    0.5 nieuwe functies
    scrollspy, carousel, indeling site

--- js/scripts.js
+++ js/scripts.js
@@ -20,6 +20,7 @@ const backgroundMap = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z
 
   const hash = new L.Hash(map1);
 
+
   var map = new maplibregl.Map({
     container: 'maplibre', // container id
     style: 'https://api.maptiler.com/maps/72f90690-1049-460a-8880-8967d62c3738/style.json?key=XdvXK2DRJHPZ6oW8ndUI', // style URL
@@ -28,4 +29,57 @@ const backgroundMap = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z
     hash: true,
 });
 
+map.on('load' , function(){
+  console.log(map.getStyle())
+
+  map.setPaintProperty('Water','fill-color','#009ffa');
+
+})
+
+var selectedbutton = document.querySelector("#KleurButton");
+
+selectedbutton.addEventListener("click", KleurVerander);
+
+function KleurVerander()
+{
+    map.setPaintProperty('Water','fill-color','#ffff00')
+}
+
+
 map.addControl(new maplibregl.FullscreenControl({container: document.querySelector('body')}));
+
+
+
+
+
+
+
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
